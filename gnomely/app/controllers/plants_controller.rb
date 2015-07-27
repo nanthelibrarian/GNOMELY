294e13d2ffcce2de6747f3ebf50fce15f802e49e
Nancy Wood <nancywood13@gmail.com>
2015-07-27 16:07:48 -0400

    create plant controller and start plant views

---
+++ gnomely/app/controllers/plants_controller.rb
@@ -0,0 +1,50 @@
+class PlantsController < ApplicationController
+  
+  def index
+    @plants = Plant.all 
+
+  end
+  
+  def show
+    @plant = Plant.find(params[:id])    
+  end
+  
+  def new
+    @plant = Plant.new
+  end
+
+  def edit
+    @plant = Plant.find(params[:id])
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+      if @plant.save
+        redirect_to plants_url
+      else
+        render :new 
+      end   
+  end
+
+  def update
+    @plant = Plant.find(params[:id])
+    if @plant.update_attributes(plant_params)
+      redirect_to plant_path(@plant)
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy 
+    redirect_to plants_path 
+  end
+
+  private
+
+  def plant_params 
+    params.require(:plant).permit(:name, :description, :notes)
+  end 
+
+end
