52be62603a26ada1595646498b3fce254e5f1867
RHEA GANAPATHY <rheasg@gmail.com>
2015-07-28 15:29:02 -0400

    sorcery gem added and users model modified

---
+++ db/migrate/20150728190644_add_sorcery_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddSorceryColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :crypted_password, :string
+    add_column :users, :salt, :string
+
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727193631) do
+ActiveRecord::Schema.define(version: 20150728191413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,12 @@ ActiveRecord::Schema.define(version: 20150727193631) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "crypted_password"
+    t.string   "salt"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
