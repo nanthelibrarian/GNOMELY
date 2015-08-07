70939d27c84d8809cb3409dd8a0845ca81855e6e
Nancy Wood <nancywood13@gmail.com>
2015-08-07 15:56:22 -0400

    update last_moisture_reading_at field in send_moisture_reading

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -54,6 +54,7 @@ class PlantsController < ApplicationController
   def send_moisture_reading
     @plant = Plant.find(params[:id])
     @plant.update_attributes(moisture_reading)
+    @plant.update_attributes({last_moisture_reading_at: DateTime.now}) 
   end
 
   private
