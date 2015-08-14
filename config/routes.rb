118ff9086e429920627648f775f72d147b3bc04f
Mina Mikhail <mina@fightingtheboss.com>
2015-08-14 16:11:57 -0400

    Make logging out a GET request rather than a POST

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'user_sessions#new', :as => :login
   get 'signup' => 'users#new', :as => :signup
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'logout' => 'user_sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
